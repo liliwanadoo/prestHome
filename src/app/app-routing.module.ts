652411f9f51867c941251094aa933fca7191ab29
pb <pyhair.b@gmail.com>
2019-10-14 11:50:30 +0200

    create profil + show partie et lien create usr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { AddPrestataireComponent } from './pages/add-prestataire/add-prestataire.component';
 
 
 const routes: Routes = [
   { path: '', component: AccueilComponent},
-  { path: 'sign', component: SignInComponent}
+  { path: 'sign', component: SignInComponent},
+  { path: 'fillprofil', component: AddPrestataireComponent}
 ];
 
 @NgModule({
--- src/app/shared/ui/material/material.module.ts
+++ src/app/shared/ui/material/material.module.ts
@@ -7,6 +7,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatOptionModule } from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [],
@@ -18,7 +19,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatButtonModule,
     MatFormFieldModule,
     MatOptionModule,
-    MatSelectModule
+    MatSelectModule,
+    MatRadioModule
 
   ],
   exports: [
@@ -28,7 +30,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatButtonModule,
     MatFormFieldModule,
     MatOptionModule,
-    MatSelectModule
+    MatSelectModule,
+    MatRadioModule
 
   ]
 })
