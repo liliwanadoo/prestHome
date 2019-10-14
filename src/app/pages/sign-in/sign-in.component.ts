82b369b2dca5569773edce49db3d0926fd0c876a
pb <pyhair.b@gmail.com>
2019-10-14 09:27:23 +0200

    petites correctrion style de signin

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -54,23 +54,23 @@ ngOnInit() {
 }
 
 public submit() {
-  if(this.userForm.valid){
+  if (this.userForm.valid) {
   const newUsr: User = new User();
   newUsr.email = this.email.value;
   newUsr.password = this.password.value;
   this.http.postUsr(newUsr).pipe(first())
-  .subscribe((data: HttpResponse<number>) =>
-    {
-      if(data.status === 201) {
+  .subscribe((data: HttpResponse<number>) => {
+      if (data.status === 201) {
         this.router.navigate(['']);
       }
-    },error => {
-      console.log("not working sorry");
+    }, error => {
+      console.log( 'not working sorry' );
     }
 
 
     );
-
+    // TODO : probablement un commentaire pour dire que le compte a bien été créé
+  this.router.navigate(['']);
   }
 }
 }
