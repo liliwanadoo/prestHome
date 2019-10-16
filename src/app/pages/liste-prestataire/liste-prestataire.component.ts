be8ac3b7317920ab322c93c05a68b08868360ce0
Felice <liliwanadoo@yahoo.fr>
2019-10-16 16:14:28 +0200

    affichage de la categorie 7 mocke dans le httpClient

--- src/app/pages/liste-prestataire/liste-prestataire.component.ts
+++ src/app/pages/liste-prestataire/liste-prestataire.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PrestataireList } from 'src/app/models/prestataire-list';
 import { Prestataire } from 'src/app/models/prestataire';
+import { MaCategorie } from 'src/app/models/ma-categorie';
+import { Categorie } from 'src/app/models/categorie';
 
 @Component({
   selector: 'app-liste-prestataire',
@@ -10,11 +12,16 @@ import { Prestataire } from 'src/app/models/prestataire';
 export class ListePrestataireComponent implements OnInit {
   @Input() all: boolean;
   public prestataires: Array<Prestataire>;
+  public categorie: Categorie;
   public prest: Prestataire = new Prestataire();
 
-  constructor(private collection: PrestataireList) { }
+  constructor(private collection: PrestataireList, private maCategorie: MaCategorie) { }
 
   ngOnInit() {
+    this.maCategorie.getCollection().then((cats: Categorie) => {
+      this.categorie = cats;
+    });
+
     this.collection.getCollection(this.all).then((prests: Array<Prestataire>) => {
       this.prestataires = prests;
    });
