c97d6b90299781146f416b1151b8ff3ced2ba588
Felice <liliwanadoo@yahoo.fr>
2019-10-16 16:59:26 +0200

    affichage de Vendargues mocke dans le httpClient

--- src/app/pages/liste-prestataire/liste-prestataire.component.ts
+++ src/app/pages/liste-prestataire/liste-prestataire.component.ts
@@ -3,6 +3,8 @@ import { PrestataireList } from 'src/app/models/prestataire-list';
 import { Prestataire } from 'src/app/models/prestataire';
 import { MaCategorie } from 'src/app/models/ma-categorie';
 import { Categorie } from 'src/app/models/categorie';
+import { Coordonnee } from 'src/app/models/coordonnee';
+import { MaVilleCP } from 'src/app/models/ma-villeCP';
 
 @Component({
   selector: 'app-liste-prestataire',
@@ -13,13 +15,18 @@ export class ListePrestataireComponent implements OnInit {
   @Input() all: boolean;
   public prestataires: Array<Prestataire>;
   public categorie: Categorie;
+  public coordonnee: Coordonnee;
   public prest: Prestataire = new Prestataire();
 
-  constructor(private collection: PrestataireList, private maCategorie: MaCategorie) { }
+  constructor(private collection: PrestataireList, private maCategorie: MaCategorie, private maVilleCP: MaVilleCP) { }
 
   ngOnInit() {
     this.maCategorie.getCollection().then((cats: Categorie) => {
       this.categorie = cats;
+    });
+
+    this.maVilleCP.getCollection().then((coord: Coordonnee) => {
+      this.coordonnee = coord;
     });
 
     this.collection.getCollection(this.all).then((prests: Array<Prestataire>) => {
