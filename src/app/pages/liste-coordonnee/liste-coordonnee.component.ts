d19486f54f560647fba54f5f921e861a10c859e4
Felice <liliwanadoo@yahoo.fr>
2019-10-17 10:47:04 +0200

    rien

--- src/app/pages/liste-coordonnee/liste-coordonnee.component.ts
+++ src/app/pages/liste-coordonnee/liste-coordonnee.component.ts
@@ -16,14 +16,10 @@ export class ListeCoordonneeComponent implements OnInit {
 
   constructor(private collection: CoordonneeList) { }
   ngOnInit(): void {
-<<<<<<< HEAD
+
       console.log(this.allCoord ? 'Tous' : 'Restreint');
       this.collection.getCollection(this.allCoord).then((coords: Array<Coordonnee>) => {
 
-=======
-      console.log(this.all ? 'Tous' : 'Restreint');
-      this.collection.getCollection(this.all).then((coords: Array<Coordonnee>) => {
->>>>>>> 72ad0f62d3b7ef8bb861e636ffb71dec5a186a62
        this.coordonnees = coords;
        console.log('Liste : ' + JSON.stringify(this.coordonnees));
     });
